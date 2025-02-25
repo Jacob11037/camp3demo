27222b34ece18b1abf1ce845470940d5251b9f31
aneesh <aneeshusunique@gmail.com>
2025-02-25 12:42:05 +0530

    updated startup

--- models/startapp.py
+++ models/startapp.py
@@ -1,2 +1,15 @@
 class startapp:
-    pass
+    def __init__(self, username, password, customer):
+        self.username = username
+        self.password = password
+        self.customer = customer
+
+    def display_info(self):
+        print(f"Admin Username: {self.username}")
+        print(f"Managing Customer: {self.customer}")
+
+
+admin1 = Admin("admin123", "securepass", "John Doe")
+
+
+admin1.display_info()
